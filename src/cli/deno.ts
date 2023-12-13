121e171fbcdac5ae6763ce5cd10e6f4eab51f109
Danilo Valente <danilo@valente.work>
2023-12-13 14:14:41 +0100

    feat(cli/deno): allow CLI to run commands cache, fmt, lint and test to run at once

--- src/cli/deno.ts
+++ src/cli/deno.ts
@@ -32,32 +32,63 @@ const buildCommand = (cmd: Cmd, description: string) =>
     .arguments('[module:egg] [...args:string]')
     .option('-e, --shell <shell:shell>', 'Export command as a shell script')
     .stopEarly()
+    .action(async ({ shell }, module, ...args): Promise<Deno.CommandStatus> => {
+      const compy = await getCompy();
+
+      const runOrExportModule = async (moduleName: string) => {
+        const native = await loadNative(compy, [cmd], moduleName, args);
+
+        return runOrExport(native, shell);
+      };
+
+      if (module) {
+        return runOrExportModule(module);
+      }
+
+      const modules = await getEggs();
+
+      for (const module of modules) {
+        const status = await runOrExportModule(module);
+        if (!status.success) {
+          return status;
+        }
+      }
+
+      return {
+        success: true,
+        code: 0,
+        signal: null,
+      };
+    });
+
+const buildModuleCommand = (cmd: Cmd, description: string) =>
+  new Command()
+    .name(cmd)
+    .description(description)
+    .type('egg', new EggType())
+    .type('shell', shellType)
+    .arguments('<module:egg> [...args:string]')
+    .option('-e, --shell <shell:shell>', 'Export command as a shell script')
+    .stopEarly()
     .action(async ({ shell }, module, ...args) => {
       const compy = await getCompy();
-      if (module === undefined) {
-        const modules = await getEggs();
-        for (const module of modules) {
-          const native = await loadNative(compy, [cmd], module, args);
-          await runOrExport(native, shell);
-        }
-        return;
-      }
 
       const native = await loadNative(compy, [cmd], module, args);
 
       return await runOrExport(native, shell);
     });
 
-export const cache = buildCommand('cache', 'Run \'deno cache\' on a module');
-export const fmt = buildCommand('fmt', 'Run \'deno fmt\' on a module');
-export const lint = buildCommand('lint', 'Run \'deno lint\' on a module');
-export const test = buildCommand('test', 'Run \'deno test\' on a module');
-export const start = buildCommand('start', 'Run \'deno run\' on a module with \'start\' config');
-export const dev = buildCommand('dev', 'Run \'deno run\' on a module with \'dev\' config');
+export const cache = buildCommand('cache', `Run 'deno cache' on a module`);
+export const fmt = buildCommand('fmt', `Run 'deno fmt' on a module`);
+export const lint = buildCommand('lint', `Run 'deno lint' on a module`);
+export const test = buildCommand('test', `Run 'deno test' on a module`);
+
+export const start = buildModuleCommand('start', `Run 'deno run' on a module with 'start' config`);
+export const dev = buildModuleCommand('dev', `Run 'deno run' on a module with 'dev' config`);
 
 export const run = new Command()
   .name('run')
-  .description('Run \'deno run\' on a module')
+  .description(`Run 'deno run' on a module`)
   .type('egg', new EggType())
   .type('shell', shellType)
   .arguments('<module:egg> <task:string> [...args:string]')
